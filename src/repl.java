34abe39366b903f14c84d34867435456178a7906
Einhander0000 <kevin0bui@gmail.com>
2016-05-09 18:21:47 -0700

    Basic Parser is Finished
    Now allocated properly for basic functions, still needs to revolve basic
    arithmetic + trigonometry

--- src/repl.java
+++ src/repl.java
@@ -1,7 +1,10 @@
+import com.sun.deploy.util.SystemUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collector;
 
 public class repl {
 
@@ -10,8 +13,8 @@ public class repl {
     static Stack operators = new Stack();
 
 
+
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World");
         run();
     }
 
@@ -23,16 +26,30 @@ public class repl {
         }
         System.out.println("THIS IS VARIABLES: " +  Arrays.toString(variables.toArray()));
         System.out.println("THIS IS SYMBOLS: " + Arrays.toString(symbols.toArray()));
+
+        System.out.println("LOOK SIZE: " + variables.size());
+
+//        for(int i = 0; i < variables.size(); i++){
+//            if(variables.peek() instanceof  Integer){
+//                System.out.println("Look an integer: " + variables.get(i));
+//            }
+//        }
     }
 
     public static void tokenizer(String token) {
         double answer = 0;
+        boolean integerFlag = false; //Used to calculate integer places
+        boolean integerExists = false;
+        int currentInt = 0;
+
 
         /**
          * This is for more complex tokens
          */
         for (int i = 0; i < token.length(); i++) {
-            boolean integerFlag = false; //Used to calculate integer places
+            System.out.println("CurrentCHAR: " + token.charAt(i));
+            System.out.println("CURRENT INTEGER: " + currentInt);
+
             char c = token.charAt(i);
             //Process char
 
@@ -40,36 +57,82 @@ public class repl {
                 variables.add(c);
             }
 
+            /*returns the integer value of number and adds it to stack*/
             if("1234567890".indexOf(c) != -1){
-                variables.add(c);
+                if(integerFlag == true) {
+                    /* Checks if it is greater than 9. I.E: 10, 1000, etc. Decimal place calculation
+                     * If current index is an integer, check the rest of the token to see if there are any other numbers
+                     * that fits the criteria
+                     * */
+                    System.out.println("ENTERED IF FIRST IF STATEMENT");
+                    System.out.println("OLD INT(FIRST IF): " + currentInt);
+                    currentInt = (currentInt * 10) + Character.getNumericValue(c);
+                    System.out.println("NEW CURRENT INT (FIRST IF): " + currentInt);
+                    System.out.println("FIRST IF: CurrentInt: " + currentInt);
+                    //variables.add(currentInt);
+
+                }else {
+
+                    System.out.println("ENTERED IN ELSE STATEMENT");
+                    System.out.println("ELSE STATEMENT: Character.getNumericValue(c): " + Character.getNumericValue(c));
+                    currentInt = Character.getNumericValue(c);
+                    System.out.println("ELSE STATEMENT: NEW CURRENT INT OLD LOOP: " + currentInt);
+                    integerFlag = true;
+                    integerExists = true;
+                }
             }
 
             switch (c) {
                 case '+':
                     symbols.push("+");
+                    System.out.println("THIS IS THE FINAL INT BEFORE PUSHING: " + currentInt);
+                    if(currentInt != 0 && integerExists){
+                        variables.push(currentInt);
+                        currentInt = 0;
+                    }
                     break;
                 case '-':
                     symbols.push("-");
+                    if(currentInt != 0 && integerExists){
+                        variables.push(currentInt);
+                        currentInt = 0;
+                    }
                     break;
                 case '*':
                     symbols.push("*");
+                    if(currentInt != 0 && integerExists){
+                        variables.push(currentInt);
+                        currentInt = 0;
+                    }
                     break;
                 case '^':
                     symbols.push("^");
+                    if(currentInt != 0 && integerExists){
+                        variables.push(currentInt);
+                        currentInt = 0;
+                    }
                     break;
                 case '=':
                     symbols.push("=");
+                    if(currentInt != 0 && integerExists){
+                        variables.push(currentInt);
+                        currentInt = 0;
+                    }
                     break;
-                default:
+                case ' ':
+                    if(currentInt != 0 && integerExists){
+                        variables.push(currentInt);
+                        currentInt = 0;
+                    }
                     break;
             }
-
-
-
+            System.out.println("END ONE FOR LOOP");
         }
-
-
-    System.out.println("I'm done!");
+        if(currentInt != 0 && integerExists){
+            variables.push(currentInt);
+            currentInt = 0;
+        }
+        System.out.println("This is the end dear friends");
     }
 
     public static void popper(){
@@ -77,6 +140,7 @@ public class repl {
 
         /*If this variable is instance of:*/
         if(variables.peek() instanceof  Integer){
+
 
         }
     }
