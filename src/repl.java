a541dedd347dc49be24fe5e304ddb227b09256a0
Einhander0000 <kevin0bui@gmail.com>
2016-05-12 02:30:38 -0700

    Implemented Basic Arithmetic Calculations
    Now can do basic arithmetics.
    Still needs:
    -Still need user-defined functions
    -Trig functions

--- src/repl.java
+++ src/repl.java
@@ -7,6 +7,14 @@ import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.Collector;
 
+
+/*
+Kevin Bui - 007684183
+CS152 Programming Paradigms Assignment 3
+Instructor: Thaddeus Aid
+Parser Logic take elements from: http://www.sunshine2k.de/coding/java/SimpleParser/SimpleParser.html
+
+ */
 public class repl {
 
     static Stack symbols = new Stack();
@@ -14,10 +22,13 @@ public class repl {
 
     public static void main(String[] args) throws IOException {
         Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter input:");
         while (myScanner.hasNextLine()) {
+
             String inputA = myScanner.nextLine();
             System.out.println("CURRENT STRING: " + inputA);
             System.out.println("POSTFIX: " + advancedTokenizer(inputA));
+            System.out.println("OUTPUT: " + evaluatePostFix(advancedTokenizer(inputA)));
         }
     }
 
@@ -118,16 +129,6 @@ public class repl {
         System.out.println("This is the end dear friends");
     }
 
-    public static void startPopper() {
-        int output = 0;
-
-        /*If this variable is instance of:*/
-        if (variables.peek() instanceof Integer) {
-
-
-        }
-    }
-
     public static String advancedTokenizer(String input) throws ParserException {
         StringBuffer postFix = new StringBuffer();
         Stack operators = new Stack();
@@ -136,12 +137,10 @@ public class repl {
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             System.out.println("Current c: " + c);
+
             /*
             * This accounts for numbers > 9 I.E: 10, 1000, 12345767, etc.
             */
-
-
-
             if (c >= '0' && c <= '9') {
                 // process numericals
                 while ((c >= '0' && c <= '9') || c == '.') {
@@ -158,8 +157,6 @@ public class repl {
                 postFix.append(" ");
                 //i--;
             }
-
-
 
             switch (c) {
                 case ')':
@@ -192,24 +189,21 @@ public class repl {
                     postFix.append(" ");
                     break;
                 case '*':
-                    if (!operators.empty() && (operators.peek().equals('*') || operators.peek().equals('/')))
-                    {
+                    if (!operators.empty() && (operators.peek().equals('*') || operators.peek().equals('/'))) {
                         postFix.append(operators.pop());
                     }
                     operators.push(c);
                     postFix.append(" ");
                     break;
                 case '/':
-                    if (!operators.empty() && (operators.peek().equals('*') || operators.peek().equals('/')))
-                    {
+                    if (!operators.empty() && (operators.peek().equals('*') || operators.peek().equals('/'))) {
                         postFix.append(operators.pop());
                     }
                     operators.push(c);
                     postFix.append(" ");
                     break;
                 case '^':
-                    if (!operators.empty() && operators.peek().equals('^'))
-                    {
+                    if (!operators.empty() && operators.peek().equals('^')) {
                         postFix.append(operators.pop());
                     }
                     operators.push(c);
@@ -221,7 +215,7 @@ public class repl {
                     * */
                     break;
                 case ' ':
-                    if(!spacer){
+                    if (!spacer) {
                         spacer = true;
                         postFix.append(" ");
                         break;
@@ -229,28 +223,89 @@ public class repl {
                     spacer = false;
                     break;
             }
-
-
             /*If there exists a illegal character, throw a Parser Exception*/
             if (("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-^*/()1234567890 ".indexOf(c) == -1) && (c != '\u0000')) {
                 throw new ParserException("Invalid symbol: " + c);
             }
 
 
-
         }
-
         while (!operators.empty()) {
             postFix.append(operators.pop());
             postFix.append(" ");
         }
         // delete the space character at the end of the string wrongly added in above while-loop
         postFix.deleteCharAt(postFix.length() - 1);
-
-
         return postFix.toString();
-
     }
 
+    public static double evaluatePostFix(String inputString) throws ParserException {
+        Stack operators = new Stack();
+        char c;
+        double x = 0;
+        Scanner postFixScanner = new Scanner(inputString);
+        while (postFixScanner.hasNext()) {
+            String input = postFixScanner.next();
+            for (int i = 0; i < input.length(); i++) {
+                c = input.charAt(i);
+                x = 0;
+                switch (c) {
+                    case '+':
+                        double x1 = Double.valueOf(operators.pop().toString());
+                        double x2 = Double.valueOf(operators.pop().toString());
+                        x = x2 + x1;
+                        operators.push(x);
+                        break;
+                    case '-':
+                        double x3 = Double.valueOf(operators.pop().toString());
+                        double x4 = Double.valueOf(operators.pop().toString());
+                        x = x4 - x3;
+                        operators.push(x);
+                        break;
+                    case '*':
+                        double x5 = Double.valueOf(operators.pop().toString());
+                        double x6 = Double.valueOf(operators.pop().toString());
+                        x = x6 * x5;
+                        operators.push(x);
+                        break;
+                    case '/':
+                        double x7 = Double.valueOf(operators.pop().toString());
+                        double x8 = Double.valueOf(operators.pop().toString());
+                        x = x8 / x7;
+                        operators.push(x);
+                        break;
+                    case '^':
+                        double x9 = Double.valueOf(operators.pop().toString());
+                        double x10 = Double.valueOf(operators.pop().toString());
+                        x = Math.pow(x9, x10);
+                        operators.push(x);
+                        break;
+                }
 
+                if (c >= '0' && c <= '9') {
+                    String sub = input.substring(i);
+                    int j = 0;
+                    for (j = 0; j < sub.length(); j++) {
+                        if (sub.charAt(j) == ' ') {
+                            sub = sub.substring(0, j);
+                        }
+                    }
+                    // 'sub' contains now just the number
+                    try {
+                        x = Double.parseDouble(sub);
+                    } catch (NumberFormatException ex) {
+                        throw new ParserException("String to number parsing exception: " + input);
+                    }
+                    operators.push(x);
+                    // go on with next token
+                    i += j - 1;
+                }
+            }
+        }
+        return x;
+    }
 }
+
+
+
+
